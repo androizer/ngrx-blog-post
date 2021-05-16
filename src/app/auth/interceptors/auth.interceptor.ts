38f27d99ae8eba27cd928f15312f31911d3f5afb
Akki Mahajan <iam.akkimahajan@gmail.com>
2021-05-17 01:25:22 +0530

    Add refresh token functionality

--- src/app/auth/interceptors/auth.interceptor.ts
+++ src/app/auth/interceptors/auth.interceptor.ts
@@ -1,24 +1,69 @@
 import {
+  HttpErrorResponse,
   HttpEvent,
   HttpHandler,
   HttpInterceptor,
   HttpRequest,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
+import { catchError, concatMap, filter, switchMap, take } from 'rxjs/operators';
+
 import { AuthService } from '../../core/services';
 
 export class AuthInterceptor implements HttpInterceptor {
   constructor(private readonly authService: AuthService) {}
+
+  readonly tokenBSubject = new BehaviorSubject<string>(null);
+  isTokenRefreshing = false;
+
+  get isTokenExpired() {
+    return this.authService.isTokenExpired();
+  }
+
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
+    return next.handle(this.attachToken(req)).pipe(
+      catchError((err) => {
+        if (
+          err instanceof HttpErrorResponse &&
+          err.status === 401 &&
+          this.isTokenExpired
+        ) {
+          return this.handleRefreshToken(req, next);
+        }
+        return throwError(err);
+      })
+    );
+  }
+
+  attachToken(req: HttpRequest<any>) {
     const accessToken = this.authService.getAccessToken();
-    if (!req.headers.get('Authorization')) {
-      req = req.clone({
-        headers: req.headers.set('Authorization', `Bearer ${accessToken}`),
-      });
+    req = req.clone({
+      headers: req.headers.set('Authorization', `Bearer ${accessToken}`),
+    });
+    return req;
+  }
+
+  handleRefreshToken(req: HttpRequest<any>, next: HttpHandler) {
+    if (!this.isTokenRefreshing) {
+      this.isTokenRefreshing = true;
+      this.tokenBSubject.next(null);
+
+      return this.authService.refreshToken().pipe(
+        concatMap((payload) => {
+          this.isTokenRefreshing = false;
+          this.tokenBSubject.next(payload.accessToken);
+          return next.handle(this.attachToken(req));
+        })
+      );
+    } else {
+      return this.tokenBSubject.pipe(
+        filter((token) => !!token),
+        take(1),
+        switchMap(() => next.handle(this.attachToken(req)))
+      );
     }
-    return next.handle(req);
   }
 }
---
+++ src/app/core/services/request-queue.service.ts
@@ -0,0 +1,55 @@
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, ReplaySubject } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+
+import { AuthService } from './auth.service';
+
+/**
+ * Will plan to use it later for better for request management.
+ */
+@Injectable()
+export class RequestQueueService implements HttpInterceptor {
+  readonly tokenBSubject = new BehaviorSubject<string>(null);
+  isTokenRefreshing = false;
+  private queue: ReplaySubject<any>[] = [];
+
+  constructor(private readonly authService: AuthService) {
+    this.tokenBSubject.asObservable().subscribe((jwt) => {
+      if (jwt) {
+        this.queue.forEach((sub$) => this.dispatchRequest(sub$));
+      }
+      this.queue.length = 0;
+    });
+  }
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const queueItem$ = new ReplaySubject();
+    const request$ = queueItem$.pipe(
+      switchMap(() => next.handle(this.attachToken(req)))
+    );
+    this.queue.push(queueItem$);
+    return request$;
+  }
+
+  dispatchRequest(nextSub$: ReplaySubject<any>) {
+    nextSub$.next();
+    nextSub$.complete();
+  }
+
+  attachToken(req: HttpRequest<any>) {
+    const accessToken = this.authService.getAccessToken();
+    req = req.clone({
+      headers: req.headers.set('Authorization', `Bearer ${accessToken}`),
+    });
+    return req;
+  }
+}
