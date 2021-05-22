c08906a9af99ddf63d67f4ff56ee18137df2e8bd
Akki Mahajan <iam.akkimahajan@gmail.com>
2021-05-22 16:28:04 +0530

    -> Add post bookmark feature -> Add relations IDs to domain models

--- src/app/blogs/components/blog-list/blog-list.component.ts
+++ src/app/blogs/components/blog-list/blog-list.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { noop, Observable, of } from 'rxjs';
-import { pluck } from 'rxjs/operators';
+import { map, pluck } from 'rxjs/operators';
 
 import { User } from '../../../auth/models';
 import { AuthService } from '../../../core/services';
+import { uuid } from '../../../core/types';
 import { Post } from '../../models';
 import { BlogService } from '../../services';
 import { ConfirmDialogComponent } from '../confirm-dialog/confirm-dialog.component';
@@ -21,8 +22,8 @@ export class BlogListComponent implements OnInit {
     private readonly dialog: MatDialog
   ) {}
 
-  allPost$: Observable<Post[]> = of([]);
-  currentUser!: User;
+  allPost: Post[] = [];
+  currentUser: User;
 
   ngOnInit() {
     this.currentUser = this.authService.getCurrentUser();
@@ -30,20 +31,31 @@ export class BlogListComponent implements OnInit {
   }
 
   getAllPosts() {
-    this.allPost$ = this.blogService
+    this.blogService
       .getAllPost({
         join: [
           { field: 'author' },
           { field: 'image' },
-          { field: 'comments', select: ['id'] },
           { field: 'author.avatar' },
         ],
         sort: { field: 'createdOn', order: 'DESC' },
       })
-      .pipe(pluck('data'));
+      .pipe(
+        pluck('data'),
+        map((posts) => {
+          return posts.map((post) => {
+            const userBookmarkIds = this.currentUser.bookmarkIds;
+            if (post.bookmarkedIds.some((id) => userBookmarkIds.includes(id))) {
+              post.isBookmarked = true;
+            }
+            return post;
+          });
+        })
+      )
+      .subscribe((posts) => (this.allPost = posts));
   }
 
-  onDelete(postId: string) {
+  onDelete(postId: uuid) {
     this.dialog.open(ConfirmDialogComponent, {
       panelClass: 'confirmation-dialog--panel',
       minWidth: '300px',
@@ -51,11 +63,30 @@ export class BlogListComponent implements OnInit {
         title: 'Confirmation',
         description: 'Are you sure you want to delete?',
         onConfirm: () => {
-          this.blogService
-            .deletePost(postId)
-            .subscribe(noop, noop, () => this.getAllPosts());
+          this.blogService.deletePost(postId).subscribe(noop, noop, () => {
+            this.allPost = this.allPost.filter((post) => post.id !== postId);
+          });
         },
       },
+    });
+  }
+
+  toggleBookmark(postId: uuid) {
+    this.blogService.toggleBookmark(postId).subscribe((payload) => {
+      this.allPost = this.allPost.map((post) => {
+        if (postId === post.id) {
+          post.isBookmarked = payload.isRemoved ? false : true;
+          if (post.isBookmarked) {
+            this.currentUser.bookmarkIds.push(payload.id);
+          } else {
+            const index = this.currentUser.bookmarkIds.findIndex(
+              (id) => id === payload.id
+            );
+            this.currentUser.bookmarkIds.splice(index, 1);
+          }
+        }
+        return post;
+      });
     });
   }
 }
--- src/app/blogs/models/post.model.ts
+++ src/app/blogs/models/post.model.ts
@@ -9,17 +9,21 @@ export class Post {
   content: string;
   votes: uuid[];
   author: User;
+  authorId: uuid;
   comments: Comment[];
+  commentIds: uuid[];
   image: Image;
   tags: string[];
-  createdBy?: string;
-  modifiedBy?: string;
+  bookmarkedIds: uuid[];
+  createdBy?: uuid;
+  modifiedBy?: uuid;
   createdOn?: Date;
   modifiedOn?: Date;
   deletedOn?: Date;
 
   // extra
   isUpvoted?: boolean;
+  isBookmarked: boolean;
 
   constructor(data = {} as Partial<Post>) {
     this.id = data.id;
@@ -27,11 +31,14 @@ export class Post {
     this.content = data.content;
     this.votes = data.votes ?? [];
     this.author = data.author && new User(data.author);
+    this.authorId = data.authorId;
     this.comments = data.comments
       ? data.comments.map((item) => new Comment(item))
       : [];
+    this.commentIds = data.commentIds;
     this.image = data.image && new Image(data.image);
     this.tags = data.tags ?? [];
+    this.bookmarkedIds = data?.bookmarkedIds ?? [];
     this.createdBy = data.createdBy;
     this.modifiedBy = data.modifiedBy;
     this.createdOn = data.createdOn;
